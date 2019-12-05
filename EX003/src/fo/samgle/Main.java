66b6ab150ee5727760110e423d0244f1c47513c4
sam223221 <58514851+sam223221@users.noreply.github.com>
2019-12-05 16:19:09 +0000

    rokning

---
+++ EX003/src/fo/samgle/Main.java
@@ -0,0 +1,18 @@
+package fo.samgle;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int y=10;
+        System.out.println(y);
+    int x=11;
+	    System.out.println(x);
+
+        System.out.println(x+y);
+    y = 11;
+        System.out.println(x+y);
+
+    }
+
+
+}
