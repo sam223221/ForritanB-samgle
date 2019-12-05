698b27d13d0477c386f21b5a0f5d9db53a176c07
sam223221 <58514851+sam223221@users.noreply.github.com>
2019-12-05 16:11:01 +0000

    rokning

---
+++ ForritanB-XXX/EX002/src/fo/samgle/Main.java
@@ -0,0 +1,13 @@
+package fo.samgle;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i = 7;
+        int i1 =20;
+        int i2 = 3;
+
+        System.out.println(i+i1*i2);
+    }
+}
