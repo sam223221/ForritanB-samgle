a96ab743dfc024355019bba5c9821757f6696fec
sam223221 <58514851+sam223221@users.noreply.github.com>
2020-01-22 14:08:37 +0000

    Sum urin

--- EX006/src/fo/samgle/Form.java
+++ EX006/src/fo/samgle/Form.java
@@ -1,6 +1,31 @@
 package fo.samgle;
 
+import javax.swing.*;
+
 public class Form {
 
-int tim;
+
+
+        public static void main(String[] args) {
+                int Dim [] = {31,28,100,500,321,626,65,9,182,26,6321,61,126,155,66} , n;
+                int i=0;
+                int teljari=0;
+                int plussa = 0;
+                int a = 0;
+
+                n = Dim.length;
+
+                for (int b = 0; b < n ; b++)
+                        if (i < n) {
+                                a = a + Dim[teljari];
+                                plussa = plussa + a;
+                                teljari++;
+                        }
+                System.out.println("hettar er alt samantalt " + plussa);
+
+
+
+        }
+
+
 }
