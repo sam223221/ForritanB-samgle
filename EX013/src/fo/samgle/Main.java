5f43f762ebe68a42feaf1afade2f5a78132c7fd3
sam223221 <58514851+sam223221@users.noreply.github.com>
2020-01-30 16:42:07 +0000

    ikki ligut enn

---
+++ EX013/src/fo/samgle/Main.java
@@ -0,0 +1,27 @@
+package fo.samgle;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String heystak = JOptionPane.showInputDialog("er tað ein lija");
+        String needel = JOptionPane.showInputDialog("er tað i?");
+
+        int lenghthey = heystak.length();
+        int lenghtneed = needel.length();
+
+
+
+        for(int i = lenghtneed; lenghtneed >= lenghthey ;i++){
+
+            String test = heystak.charAt(lenghtneed);
+
+            if(heystak.charAt(lenghtneed)){
+
+            }
+        }
+
+    }
+}
