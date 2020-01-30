775e98f2389e11ad69a913bc25a9a34414587f91
sam223221 <58514851+sam223221@users.noreply.github.com>
2020-01-30 16:04:19 +0000

    uppgáva 12

---
+++ EX012/src/fo/samgle/Main.java
@@ -0,0 +1,25 @@
+package fo.samgle;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String name = JOptionPane.showInputDialog("gev eitt tal").toLowerCase(), revers = "";
+
+        int lenght = name.length();
+
+        for(int i = lenght - 1 ; i >= 0 ; i--){
+
+            revers = revers + name.charAt(i);
+
+        }
+        if(name.equals(revers)){
+            System.out.println("hettar er ein pallinom");
+        }else{
+            System.out.println("nigga you gay??");
+
+        }
+    }
+}
