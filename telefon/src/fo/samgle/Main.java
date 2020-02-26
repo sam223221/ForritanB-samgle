debce16e428be4510aebad8d1b166c6c683e648c
sam223221 <58514851+sam223221@users.noreply.github.com>
2020-02-26 13:32:01 +0000

    Telefon

---
+++ telefon/src/fo/samgle/Main.java
@@ -0,0 +1,48 @@
+package fo.samgle;
+
+import javax.swing.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        telefon T1 = new telefon();
+        T1.EigaraNavn = "Sjúrður";
+        T1.Freamleiðari = "Kina";
+        T1.Model = "Apple";
+        T1.SkermStødd = 20;
+
+        telefon T2 = new telefon();
+        T2.EigaraNavn = "Hans";
+        T2.Freamleiðari = "Japan";
+        T2.Model = "Samsung";
+        T2.SkermStødd = 10;
+
+        telefon T3 = new telefon();
+        T3.EigaraNavn = "Magni";
+        T3.Freamleiðari = "Intel";
+        T3.Model = "RED";
+        T3.SkermStødd = 30;
+
+        ArrayList<telefon> Telefonhaldari = new ArrayList();
+        Telefonhaldari.add(T1);
+        Telefonhaldari.add(T2);
+        Telefonhaldari.add(T3);
+
+        String Input = JOptionPane.showInputDialog("hvat er navn títt");
+
+        for(int i = 0; i < Telefonhaldari.size(); i++){
+            telefon t = Telefonhaldari.get(i);
+
+            if (t.EigaraNavn.equals(Input)){
+
+                System.out.println("Eiðari: " + t.EigaraNavn + "    framleiðari: " + t.Freamleiðari + "     Model: " + t.Model + "      skermstødd: " + t.SkermStødd);
+                return;
+            }
+
+        }
+    }
+}
---
+++ telefon/src/fo/samgle/telefon.java
@@ -0,0 +1,9 @@
+package fo.samgle;
+
+public class telefon {
+    public String Freamleiðari = "";
+    public String Model = "";
+    public String EigaraNavn = "";
+    public Integer SkermStødd = 0;
+
+}
